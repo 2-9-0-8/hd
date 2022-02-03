f930b8ae018e5ccf4355bc6fe2a89b3be6cf211a
Jerome Kenway <jeromekenway@gmail.com>
2022-02-03 22:29:21 +0000

    changes

--- pages/archive/[categorySlug]/[postSlug].tsx
+++ pages/archive/[categorySlug]/[postSlug].tsx
@@ -1,71 +1,56 @@
-import Link from 'next/link'
+import Layout from '@components/layouts/Layout'
+import { getNextStaticProps, is404 } from '@faustjs/next'
+import { GetStaticPropsContext } from 'next'
 import { client } from '@client'
-import { useRouter } from 'next/router'
-import styles from '@styles/modules/MovePost.module.css'
-import btoa from 'btoa'
-import useSWR from 'swr'
+import styles from '@styles/modules/Post.module.css'
+import { CategoryNav, MovePost } from '@components/includes'
+import Image from 'next/image'
 
-export default function MovePost() {
+
+export default function Page() {
   const { usePost } = client
   const post = usePost()
-  const { query = {} } = useRouter()
-  const { categorySlug } = query
-  const { posts } = client.useQuery()
-  //const currentPaginationCursor = btoa( `arrayconnection:${post.databaseId}` )
-  const { data, error } = useSWR(`arrayconnection:${post.databaseId}`, 
-    {
-      revalidateOnMount: true
-    }
-  )
-  
-  const currentPaginationCursor = btoa(data)
-
-  const previous = posts({
-    first: 1,
-    before: currentPaginationCursor,
-    where: { categoryName: `${categorySlug}` },
-  }).nodes[0]?.slug
-
-  const next = posts({
-    first: 1,
-    after: currentPaginationCursor,
-    where: { categoryName: `${categorySlug}` },
-  }).nodes[0]?.slug
-
-  if (error) <p>Loading failed...</p>
-  if (!currentPaginationCursor) <p>Loading...</p>
 
   return (
-    <nav className={styles.move}>
-      { previous && 
-        <Link href={`/archive/${categorySlug}/${previous}`}>
-          <a className={styles.previous}>
-            <span>
-              <svg width="25" xmlns="http://www.w3.org/2000/svg" height="25" fill="black" viewBox="0 0 6.3499999 6.3500002">
-                <g id="layer1" transform="translate(0 -290.65)">
-                  <path id="path9429" d="m2.2580394 291.96502a.26460982.26460982 0 0 0 -.1741496.46871l1.6190225 1.38699-1.6190225 1.38648a.26460982.26460982 0 1 0 .3436483.40049l1.8536335-1.58595a.26460982.26460982 0 0 0 0-.40256l-1.8536335-1.5875a.26460982.26460982 0 0 0 -.1694987-.0667z" />
-                </g>
-              </svg>
-            </span>
-            Previous
-          </a>
-        </Link>
-      }
+    <>
+      <Layout
+        title={post?.title}
+      >        
+        <CategoryNav />
 
-      { next && 
-        <Link href={`/archive/${categorySlug}/${next}`}>
-          <a className={styles.next}>
-            Next
-            <span>
-              <svg width="25" xmlns="http://www.w3.org/2000/svg" height="25" fill="black" viewBox="0 0 6.3499999 6.3500002">
-                <g id="layer1" transform="translate(0 -290.65)">
-                  <path id="path9429" d="m2.2580394 291.96502a.26460982.26460982 0 0 0 -.1741496.46871l1.6190225 1.38699-1.6190225 1.38648a.26460982.26460982 0 1 0 .3436483.40049l1.8536335-1.58595a.26460982.26460982 0 0 0 0-.40256l-1.8536335-1.5875a.26460982.26460982 0 0 0 -.1694987-.0667z" />
-                </g>
-              </svg>
-            </span>
-          </a>
-        </Link>
-      }
-    </nav>
+        <article className={styles.post}>
+          <div className={styles.image}>
+            <Image
+              src={post?.featuredImage?.node?.sourceUrl()}
+              alt={post?.featuredImage?.node?.altText}
+              width={post?.featuredImage?.node?.mediaDetails?.width}
+              height={post?.featuredImage?.node?.mediaDetails?.height}
+              objectFit="contain"
+            />
+          </div>
+          <div className={styles.content}>
+            <h1>{post?.title()}</h1>
+            <div dangerouslySetInnerHTML={{ __html: post?.content() ?? '' }} />
+          </div>
+        </article>
+        
+        <MovePost />
+      </Layout>
+    </>
   )
+}
+
+export async function getStaticProps(context: GetStaticPropsContext) {
+  return getNextStaticProps(context, {
+    Page,
+    client,
+    notFound: await is404(context, { client }),
+  })
+}
+
+export function getStaticPaths() {
+  return {
+    paths: [],
+    fallback: 'blocking',
+  }
 }
